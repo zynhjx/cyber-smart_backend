7a1c0c5dee14ac9eed07352c54efc1f76ea47f98
lynxairu <lynxairu.ichiro@gmail.com>
2025-09-22 10:41:09 +0800

    debugging 502

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
