7958c7a8eb47d2e40ee8417dc3db2c2ca562c465
lynxairu <lynxairu.ichiro@gmail.com>
2025-09-22 16:14:47 +0800

    add session manager

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.use(session({
   }
 }));
 
+function requireLogin(req, res, next) {
+  if (req.session.user) {
+    return next();
+  } else {
+    res.status(401).json({ success: false, message: "Not logged in" });
+  }
+}
+
+app.get("/dashboard-data", requireLogin, (req, res) => {
+  res.json({ 
+    username: req.session.user.username,
+    loggedIn: true
+  });
+});
 
 
 app.get("/", (req, res) => {
@@ -42,6 +56,13 @@ app.post("/login", async (req, res) => {
         const result = await pool.query("SELECT * FROM users WHERE username = $1", [req.body.username])
 
         if (result.rows.length === 0) {
+
+            req.session.user = {
+              id: result.rows[0].id,
+              username: result.rows[0].username,
+              email: result.rows[0].email
+            };
+
             res.status(401).json({ success: false, message: "User not found" });
             return;
         }
